3eb7a7aefa92b9661221530414df500709a89e2a
letrend <simon.trendel@tum.de>
2015-11-26 23:43:19 +0100

    got rid of extra logging header, because ros already has easy to use logging macros

--- src/hw_controller/src/singleJoint_controller.cpp
+++ src/hw_controller/src/singleJoint_controller.cpp
@@ -12,32 +12,32 @@ public:
     bool init(hardware_interface::PositionJointInterface* hw, ros::NodeHandle &n)
     {
 	// get joint name from the parameter server
-	std::string joint_name;
 	if (!n.getParam("joint_name", joint_name)){
-	    ROS_ERROR("Could not find joint name");
+	    ROS_FATAL("Could not find joint name");
 	    return false;
 	}
 	// get the joint object to use in the realtime loop
 	joint_ = hw->getHandle(joint_name);  // throws on failure
-	printf("PositionController initialized\n");
+	ROS_INFO("PositionController for %s initialized", joint_name.c_str());
 	return true;
     }
     
     void update(const ros::Time& time, const ros::Duration& period)
     {
 	float pos = joint_.getPosition();
-	printf("update, pos: %f\n",pos);
+	printf("%s update, pos: %f\n", joint_name.c_str(), pos);
 	double error = setpoint_ - joint_.getPosition();
 	joint_.setCommand(error*gain_);
     }
     
-    void starting(const ros::Time& time) { printf("starting controller, gain: %f, setpoint: %f\n",gain_,setpoint_);}
-    void stopping(const ros::Time& time) { printf("stopping controller\n");}
+    void starting(const ros::Time& time) { ROS_INFO("starting controller for %s, gain: %f, setpoint: %f", joint_name.c_str(),gain_,setpoint_);}
+    void stopping(const ros::Time& time) { ROS_INFO("stopping controller for %s", joint_name.c_str());}
     
 private:
     hardware_interface::JointHandle joint_;
     static const double gain_ = 1.25;
     static const double setpoint_ = 3.00;
+    std::string joint_name;
 };
 PLUGINLIB_EXPORT_CLASS(position_controllers_ns::PositionController, controller_interface::ControllerBase);
 }//namespace
