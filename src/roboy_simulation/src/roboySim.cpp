80aa140abdd66b9a72a5c1695bbeb6e2319eee9a
letrend <simon.trendel@tum.de>
2016-06-12 16:47:48 +0200

    removed unnecessary initialize service, will be obsoleted by asynch gui communication anyway

--- src/roboy_simulation/src/roboySim.cpp
+++ src/roboy_simulation/src/roboySim.cpp
@@ -19,105 +19,6 @@ namespace gazebo_ros_control {
 		delete cm;
 	}
 
-	bool RoboySim::initializeService(common_utilities::Initialize::Request &req,
-									 common_utilities::Initialize::Response &res) {
-		if (!unloadControllers(ControlMode::POSITION_CONTROL))
-			return false;
-		if (!unloadControllers(ControlMode::VELOCITY_CONTROL))
-			return false;
-		if (!unloadControllers(ControlMode::FORCE_CONTROL))
-			return false;
-		initialized = false;
-
-		char motorname[20];
-
-		for (uint i = 0; i < req.idList.size(); i++) {
-			sprintf(motorname, "motor%d", req.idList[i]);
-
-			// connect and register the joint state interface
-			hardware_interface::JointStateHandle state_handle(motorname, &pos[req.idList[i]], &vel[req.idList[i]],
-															  &eff[req.idList[i]]);
-			jnt_state_interface.registerHandle(state_handle);
-
-			uint ganglion = req.idList[i] / 4;
-			uint motor = req.idList[i] % 4;
-			switch ((uint) req.controlmode[i]) {
-				case 1: {
-					ROS_INFO("%s position controller ganglion %d motor %d", motorname, ganglion, motor);
-					// connect and register the joint position interface
-					hardware_interface::JointHandle pos_handle(jnt_state_interface.getHandle(motorname),
-															   &cmd[req.idList[i]]);
-					jnt_pos_interface.registerHandle(pos_handle);
-					break;
-				}
-				case 2: {
-					ROS_INFO("%s velocity controller ganglion %d motor %d", motorname, ganglion, motor);
-					// connect and register the joint position interface
-					hardware_interface::JointHandle vel_handle(jnt_state_interface.getHandle(motorname),
-															   &cmd[req.idList[i]]);
-					jnt_vel_interface.registerHandle(vel_handle);
-					break;
-				}
-				case 3: {
-					ROS_INFO("%s force controller ganglion %d motor %d", motorname, ganglion, motor);
-					// connect and register the joint position interface
-					hardware_interface::JointHandle eff_handle(jnt_state_interface.getHandle(motorname),
-															   &cmd[req.idList[i]]);
-					jnt_eff_interface.registerHandle(eff_handle);
-					break;
-				}
-				default:
-					ROS_WARN(
-							"The requested controlMode is not available, choose [1]PositionController [2]VelocityController [3]ForceController");
-					common_utilities::ControllerState msg;
-					msg.id = req.idList[i];
-					msg.state = ControllerState::UNDEFINED;
-					res.states.push_back(msg);
-					break;
-			}
-
-			if (true) { // only for ready motors
-				common_utilities::ControllerState msg;
-				msg.id = req.idList[i];
-				msg.state = ControllerState::INITIALIZED;
-				res.states.push_back(msg);
-			} else {
-				common_utilities::ControllerState msg;
-				msg.id = req.idList[i];
-				msg.state = ControllerState::UNDEFINED;
-				res.states.push_back(msg);
-			}
-		}
-		registerInterface(&jnt_state_interface);
-		string str;
-		registerInterface(&jnt_pos_interface);
-		vector<string> resources = jnt_pos_interface.getNames();
-		for (uint i = 0; i < resources.size(); i++) {
-			str.append(resources[i]);
-			str.append(" ");
-		}
-
-		registerInterface(&jnt_vel_interface);
-		resources = jnt_vel_interface.getNames();
-		for (uint i = 0; i < resources.size(); i++) {
-			str.append(resources[i]);
-			str.append(" ");
-		}
-
-		registerInterface(&jnt_eff_interface);
-		resources = jnt_eff_interface.getNames();
-		for (uint i = 0; i < resources.size(); i++) {
-			str.append(resources[i]);
-			str.append(" ");
-		}
-
-		ROS_INFO("Hardware interface initialized");
-		ROS_INFO("Resources registered to this hardware interface:\n%s", str.c_str());
-
-		initialized = true;
-		return true;
-	}
-
 	void RoboySim::Load(gazebo::physics::ModelPtr parent_, sdf::ElementPtr sdf_) {
 		ROS_INFO_STREAM_NAMED("gazebo_ros_control", "Loading gazebo_ros_control plugin");
 		// Save pointers to the model
@@ -207,15 +108,9 @@ namespace gazebo_ros_control {
 		urdf::Model urdf_model;
 		const urdf::Model *const urdf_model_ptr = urdf_model.initString(urdf_string) ? &urdf_model : NULL;
 
-		init_srv = nh.advertiseService("/roboy/initialize", &RoboySim::initializeService, this);
 		record_srv = nh.advertiseService("/roboy/record", &RoboySim::recordService, this);
 		steer_recording_sub = nh.subscribe("/roboy/steer_record", 1000, &RoboySim::steer_record, this);
 		roboy_pub = nh.advertise<common_utilities::RoboyState>("/roboy/state", 1000);
-
-		ROS_INFO("Waiting for initialization from GUI");
-		while (!initialized) {
-			usleep(10000);
-		}
 
 		if (!initSim(robot_namespace, nh, parent_model, urdf_model_ptr, transmissions)) {
 			ROS_FATAL_NAMED("gazebo_ros_control", "Could not initialize robot simulation interface");
