f53d25235882007934dc84d41cf5ebd09bf0abcc
letrend <simon.trendel@tum.de>
2016-08-12 11:58:59 +0200

    debian package

--- startController.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-rosrun controller_manager controller_manager start motor0 motor1 motor2 motor3 motor4 motor5 motor6 motor7 motor8 motor9 motor10 motor11 motor12 motor13 motor14 motor15 motor16 motor17 motor18 motor19 motor20 motor21 motor22 motor23
