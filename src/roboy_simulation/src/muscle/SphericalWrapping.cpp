df342e175719b03a8919d8c32bb2fb692da38ed0
asesselmann <anna.sesselmann@tum.de>
2016-09-13 23:02:49 +0200

    normalized vectors and changed computation of wrapping angle

--- src/roboy_simulation/src/muscle/SphericalWrapping.cpp
+++ src/roboy_simulation/src/muscle/SphericalWrapping.cpp
@@ -24,8 +24,8 @@ SphericalWrapping::SphericalWrapping(math::Vector3 point, double radius, int sta
 
 void SphericalWrapping::UpdateForcePoints()
 {
-    prevCoord = prevPoint->nextForcePoint;
-    nextCoord = nextPoint->prevForcePoint;
+    prevCoord = prevPoint->globalCoordinates;
+    nextCoord = nextPoint->globalCoordinates;
 
     stateMachine.UpdateState(prevCoord, nextCoord, globalCoordinates, radius);
 
@@ -35,50 +35,55 @@ void SphericalWrapping::UpdateForcePoints()
         prevForcePoint = nextCoord;
         nextForcePoint = prevCoord;
         previousSegmentLength = 0;
+        return;
+    } else {
+
+        //compute tangent points
+        //compute unit vectors and according length
+        double l_j1 = (prevCoord-this->globalCoordinates).GetLength();
+        math::Vector3 j1 = (prevCoord-this->globalCoordinates)/l_j1;
+        double l_j2 = (nextCoord-this->globalCoordinates).GetLength();
+        math::Vector3 j2 = (nextCoord-this->globalCoordinates)/l_j2;
+
+        //compute normal,
+        math::Vector3 normal_temp = j1.Cross(j2);
+        normal = normal_temp/normal_temp.GetLength();
+
+        //compute k1, k2
+        math::Vector3 k1 = j1.Cross(normal);
+        k1 = k1/k1.GetLength();
+        math::Vector3 k2 = normal.Cross(j2);
+        k2 = k2/k2.GetLength();
+
+        //compute length of a1, a2, b1, b2
+        double a1 = radius*radius/l_j1;
+        double a2 = radius*radius/l_j2;
+        double b1 = sqrt(radius*radius - a1*a1);
+        double b2 = sqrt(radius*radius - a2*a2);
+
+        if (stateMachine.state == StateMachine::POSITIVE)
+        {
+            this->prevForcePoint = this->globalCoordinates + a1*j1 - b1*k1;
+            this->nextForcePoint = this->globalCoordinates + a2*j2 - b2*k2;
+        }
+        else if (stateMachine.state == StateMachine::NEGATIVE)
+        {
+            this->prevForcePoint = this->globalCoordinates + a1*j1 + b1*k1;
+            this->nextForcePoint = this->globalCoordinates + a2*j2 + b2*k2;
+        }
+
+        //update revolution counter
+        double projection = (prevCoord - this->prevForcePoint).Dot(this->nextForcePoint - this->prevForcePoint);
+        stateMachine.UpdateRevCounter(projection);
+
+        //calculate the wrapping angle
+        double angle = acos(1 - (pow((this->prevForcePoint-this->nextForcePoint).GetLength(),2)/(2*radius*radius)));
+        arcAngle = 2*(boost::math::constants::pi<double>())*ceil(stateMachine.revCounter/2);
+        arcAngle += (stateMachine.revCounter % 2 == 0)?(angle):(-angle);
+
+        //calculate the lines of action and the muscle's length
+        previousSegmentLength = (prevCoord - this->prevForcePoint).GetLength() + arcAngle*radius;
     }
-
-    //compute tangent points
-    //compute unit vectors and according length
-    double l_j1 = (prevCoord-this->globalCoordinates).GetLength();
-    math::Vector3 j1 = (prevCoord-this->globalCoordinates)/l_j1;
-    double l_j2 = (nextCoord-this->globalCoordinates).GetLength();
-    math::Vector3 j2 = (nextCoord-this->globalCoordinates)/l_j2;
-
-    //compute normal,
-    normal = j1.Cross(j2);
-
-    //compute k1, k2
-    math::Vector3 k1 = j1.Cross(normal);
-    math::Vector3 k2 = normal.Cross(j2);
-
-    //compute length of a1, a2, b1, b2
-    double a1 = radius*radius/l_j1;
-    double a2 = radius*radius/l_j2;
-    double b1 = sqrt(radius*radius - a1*a1);
-    double b2 = sqrt(radius*radius - a2*a2);
-
-    if (stateMachine.state == StateMachine::POSITIVE)
-    {
-        this->prevForcePoint = this->globalCoordinates + a1*j1 - b1*k1;
-        this->nextForcePoint = this->globalCoordinates + a2*j2 - b2*k2;
-    }
-    else if (stateMachine.state == StateMachine::NEGATIVE)
-    {
-        this->prevForcePoint = this->globalCoordinates + a1*j1 + b1*k1;
-        this->nextForcePoint = this->globalCoordinates + a2*j2 + b2*k2;
-    }
-
-    //update revolution counter
-    double projection = (prevCoord - this->prevForcePoint).Dot(this->nextForcePoint - this->prevForcePoint);
-    stateMachine.UpdateRevCounter(projection);
-
-    //calculate the wrapping angle
-    double angle = acos(1 - (this->prevForcePoint-this->nextForcePoint).GetLength()/2*radius*radius);
-    arcAngle = 2*(boost::math::constants::pi<double>())*ceil(stateMachine.revCounter/2);
-    arcAngle += (stateMachine.revCounter % 2 == 0)?(angle):(-angle);
-
-    //calculate the lines of action and the muscle's length
-    previousSegmentLength = (prevCoord - this->prevForcePoint).GetLength() + arcAngle*radius + (this->nextForcePoint - nextCoord).GetLength();
 };
 
 void SphericalWrapping::CalculateForce()
