991caf3ab54d4ef11755d8a7b3bbcb67c636e033
letrend <simon.trendel@tum.de>
2017-06-21 01:12:42 +0200

    with hip IK

--- python/PaBiRoboy_dynamics_derivation.py
+++ python/PaBiRoboy_dynamics_derivation.py
@@ -18,21 +18,21 @@ upper_leg_right_frame = ReferenceFrame('R_4')
 lower_leg_right_frame = ReferenceFrame('R_5')
 #%% Angles
 print("Defining reference frames")
-theta0, theta1, theta2, theta3 = dynamicsymbols('theta0, theta1, theta2, theta3')
+theta0, theta1, theta2, theta3, phi = dynamicsymbols('theta0, theta1, theta2, theta3, phi')
 
-hip_frame.orient(inertial_frame, 'Body', [0, 0, 0], 'XYX')
-hip_frame.dcm(inertial_frame)
-
-upper_leg_left_frame.orient(hip_frame, 'Axis', (theta0, -hip_frame.z))
-simplify(upper_leg_left_frame.dcm(inertial_frame))
-
-lower_leg_left_frame.orient(upper_leg_left_frame, 'Axis', (theta1, -upper_leg_left_frame.z))
+lower_leg_left_frame.orient(inertial_frame, 'Axis', (phi, inertial_frame.z))
 simplify(lower_leg_left_frame.dcm(inertial_frame))
 
-upper_leg_right_frame.orient(hip_frame, 'Axis', (theta2, hip_frame.z))
+upper_leg_left_frame.orient(lower_leg_left_frame, 'Axis', (theta0, -lower_leg_left_frame.z))
+simplify(upper_leg_left_frame.dcm(inertial_frame))
+
+hip_frame.orient(upper_leg_left_frame, 'Axis', (theta1, -upper_leg_left_frame.z))
+hip_frame.dcm(inertial_frame)
+
+upper_leg_right_frame.orient(hip_frame, 'Axis', (theta2, -hip_frame.z))
 simplify(upper_leg_right_frame.dcm(inertial_frame))
 
-lower_leg_right_frame.orient(upper_leg_right_frame, 'Axis', (theta3, upper_leg_right_frame.z))
+lower_leg_right_frame.orient(upper_leg_right_frame, 'Axis', (theta3, -upper_leg_right_frame.z))
 simplify(lower_leg_right_frame.dcm(inertial_frame))
 #%% Points and Locations
 print("Defining kinematical chain")
@@ -47,26 +47,26 @@ ankle_right = Point('AnkleRight')
 
 lower_leg_length, upper_leg_length, hip_length = symbols('l1, l2, l3')
 
-hip_center.set_pos(origin, (0 * inertial_frame.y)+(0 * inertial_frame.x))
-hip_center.pos_from(origin).express(inertial_frame).simplify()
+ankle_left.set_pos(origin, (0 * inertial_frame.y)+(0 * inertial_frame.x))
+#ankle_left.pos_from(origin).express(inertial_frame).simplify()
 
-hip_left.set_pos(hip_center, hip_length/2 * hip_frame.x)
-hip_left.pos_from(origin).express(inertial_frame).simplify()
+knee_left.set_pos(ankle_left, lower_leg_length * lower_leg_left_frame.y)
+#knee_left.pos_from(origin).express(inertial_frame).simplify()
 
-hip_right.set_pos(hip_center, -hip_length/2 * hip_frame.x)
-hip_right.pos_from(origin).express(inertial_frame).simplify()
+hip_left.set_pos(knee_left, upper_leg_length * upper_leg_left_frame.y)
+#hip_left.pos_from(origin).express(inertial_frame).simplify()
 
-knee_left.set_pos(hip_left, upper_leg_length * -upper_leg_left_frame.y)
-knee_left.pos_from(origin).express(inertial_frame).simplify()
+hip_center.set_pos(hip_left, hip_length/2 * hip_frame.x)
+#hip_center.pos_from(origin).express(inertial_frame).simplify()
+
+hip_right.set_pos(hip_center, hip_length/2 * hip_frame.x)
+#hip_right.pos_from(origin).express(inertial_frame).simplify()
 
 knee_right.set_pos(hip_right, upper_leg_length * -upper_leg_right_frame.y)
-knee_right.pos_from(origin).express(inertial_frame).simplify()
-
-ankle_left.set_pos(knee_left, lower_leg_length * -lower_leg_left_frame.y)
-ankle_left.pos_from(origin).express(inertial_frame).simplify()
+#knee_right.pos_from(origin).express(inertial_frame).simplify()
 
 ankle_right.set_pos(knee_right, lower_leg_length * -lower_leg_right_frame.y)
-ankle_right.pos_from(origin).express(inertial_frame).simplify()
+#ankle_right.pos_from(origin).express(inertial_frame).simplify()
 
 lower_leg_left_com_length = lower_leg_length/2
 upper_leg_left_com_length = upper_leg_length/2
@@ -81,83 +81,85 @@ upper_leg_right_mass_center = Point('U_COMright')
 lower_leg_right_mass_center = Point('L_COMright')
 
 lower_leg_left_mass_center.set_pos(ankle_left, lower_leg_left_com_length * lower_leg_left_frame.y)
-lower_leg_left_mass_center.pos_from(origin).express(inertial_frame).simplify()
+#lower_leg_left_mass_center.pos_from(origin).express(inertial_frame).simplify()
 
 upper_leg_left_mass_center.set_pos(knee_left, upper_leg_left_com_length * upper_leg_left_frame.y)
-upper_leg_left_mass_center.pos_from(origin).express(inertial_frame).simplify()
+#upper_leg_left_mass_center.pos_from(origin).express(inertial_frame).simplify()
 
 hip_mass_center.set_pos(hip_center, 0 * hip_frame.x)
-hip_mass_center.pos_from(origin).express(inertial_frame).simplify()
+#hip_mass_center.pos_from(origin).express(inertial_frame).simplify()
 
 upper_leg_right_mass_center.set_pos(knee_right, upper_leg_right_com_length * upper_leg_right_frame.y)
-upper_leg_right_mass_center.pos_from(origin).express(inertial_frame).simplify()
+#upper_leg_right_mass_center.pos_from(origin).express(inertial_frame).simplify()
 
 lower_leg_right_mass_center.set_pos(ankle_right, lower_leg_right_com_length * lower_leg_right_frame.y)
-lower_leg_right_mass_center.pos_from(origin).express(inertial_frame).simplify()
+#lower_leg_right_mass_center.pos_from(origin).express(inertial_frame).simplify()
 
 #%% Kinematical Differential Equations
-omega0, omega1, omega2, omega3 = dynamicsymbols('omega0, omega1, omega2, omega3')
+omega0, omega1, omega2, omega3, psi = dynamicsymbols('omega0, omega1, omega2, omega3, psi')
 kinematical_differential_equations = [omega0 - theta0.diff(),
                                       omega1 - theta1.diff(),
                                       omega2 - theta2.diff(),
-                                      omega3 - theta3.diff(),]
+                                      omega3 - theta3.diff(),
+                                        psi - phi.diff(),                                
+                                        ]
 
 kinematical_differential_equations
 #%% Angular Velocities
 print("Defining angular velocities")
 
-hip_frame.set_ang_vel(inertial_frame,         0 * inertial_frame.z)
-hip_frame.ang_vel_in(inertial_frame)
+lower_leg_left_frame.set_ang_vel(inertial_frame,         0 * inertial_frame.z)
+#lower_leg_left_frame.ang_vel_in(inertial_frame)
 
-upper_leg_left_frame.set_ang_vel(hip_frame,   -omega0     * inertial_frame.z)
-upper_leg_left_frame.ang_vel_in(inertial_frame)
+upper_leg_left_frame.set_ang_vel(upper_leg_left_frame,   omega0     * inertial_frame.z)
+#upper_leg_left_frame.ang_vel_in(inertial_frame)
 
-lower_leg_left_frame.set_ang_vel(upper_leg_left_frame,   -omega1     * inertial_frame.z)
-lower_leg_left_frame.ang_vel_in(inertial_frame)
+hip_frame.set_ang_vel(hip_frame,   omega1     * inertial_frame.z)
+#hip_frame.ang_vel_in(inertial_frame)
 
-upper_leg_right_frame.set_ang_vel(hip_frame,             omega2     * inertial_frame.z)
-upper_leg_right_frame.ang_vel_in(inertial_frame)
+upper_leg_right_frame.set_ang_vel(upper_leg_right_frame, omega2     * inertial_frame.z)
+#upper_leg_right_frame.ang_vel_in(inertial_frame)
 
-lower_leg_right_frame.set_ang_vel(upper_leg_right_frame, omega3     * inertial_frame.z)
-lower_leg_right_frame.ang_vel_in(inertial_frame)
+lower_leg_right_frame.set_ang_vel(lower_leg_right_frame, omega3     * inertial_frame.z)
+#lower_leg_right_frame.ang_vel_in(inertial_frame)
 #%% Linear Velocities
 print("Defining linear velocities")
 origin.set_vel(inertial_frame, 0)
 
-hip_center.set_vel(inertial_frame, 0)
+ankle_left.set_vel(inertial_frame, 0)
 
-hip_left.v2pt_theory(hip_center, inertial_frame, hip_frame)
-hip_left.vel(inertial_frame)
+knee_left.v2pt_theory(ankle_left, inertial_frame, lower_leg_left_frame)
+#knee_left.vel(inertial_frame)
 
-hip_right.v2pt_theory(hip_center, inertial_frame, hip_frame)
-hip_right.vel(inertial_frame)
+hip_left.v2pt_theory(knee_left, inertial_frame, upper_leg_left_frame)
+#hip_left.vel(inertial_frame)
+
+hip_center.v2pt_theory(hip_left, inertial_frame, hip_frame)
+#hip_center.vel(inertial_frame)
 
 hip_mass_center.v2pt_theory(hip_center, inertial_frame, hip_frame)
-hip_mass_center.vel(inertial_frame)
+#hip_mass_center.vel(inertial_frame)
 
-knee_left.v2pt_theory(hip_left, inertial_frame, upper_leg_left_frame)
-knee_left.vel(inertial_frame)
+hip_right.v2pt_theory(hip_center, inertial_frame, hip_frame)
+#hip_right.vel(inertial_frame)
 
 knee_right.v2pt_theory(hip_right, inertial_frame, upper_leg_right_frame)
-knee_right.vel(inertial_frame)
-
-ankle_left.v2pt_theory(knee_left, inertial_frame, lower_leg_left_frame)
-ankle_left.vel(inertial_frame)
+#knee_right.vel(inertial_frame)
 
 ankle_right.v2pt_theory(knee_right, inertial_frame, lower_leg_right_frame)
-ankle_right.vel(inertial_frame)
+#ankle_right.vel(inertial_frame)
 
-lower_leg_left_mass_center.v2pt_theory(knee_left, inertial_frame, lower_leg_left_frame)
-lower_leg_left_mass_center.vel(inertial_frame)
+lower_leg_left_mass_center.v2pt_theory(ankle_left, inertial_frame, lower_leg_left_frame)
+#lower_leg_left_mass_center.vel(inertial_frame)
 
-lower_leg_right_mass_center.v2pt_theory(knee_right, inertial_frame, lower_leg_right_frame)
-lower_leg_right_mass_center.vel(inertial_frame)
+lower_leg_right_mass_center.v2pt_theory(ankle_right, inertial_frame, lower_leg_right_frame)
+#lower_leg_right_mass_center.vel(inertial_frame)
 
 upper_leg_left_mass_center.v2pt_theory(knee_left, inertial_frame, upper_leg_left_frame)
-upper_leg_left_mass_center.vel(inertial_frame)
+#upper_leg_left_mass_center.vel(inertial_frame)
 
 upper_leg_right_mass_center.v2pt_theory(knee_right, inertial_frame, upper_leg_right_frame)
-upper_leg_right_mass_center.vel(inertial_frame)
+#upper_leg_right_mass_center.vel(inertial_frame)
 #%% Masses, Inertia, Rigid Bodies
 print("Defining Masses, Inertia, Rigid Bodies")
 lower_leg_mass, upper_leg_mass, hip_mass = symbols('m_L, m_U, m_H')
@@ -241,38 +243,42 @@ upper_leg_right_torque = (upper_leg_right_frame, upper_leg_right_torque_vector)
 lower_leg_right_torque = (lower_leg_right_frame, lower_leg_right_torque_vector)
 #%% Equations of Motion
 print("Calculating equations of motion")
-coordinates = [theta0, theta1, theta2, theta3]
+coordinates = [theta0, theta1, theta2, theta3, phi]
 coordinates
 
-speeds = [omega0, omega1, omega2, omega3]
+speeds = [omega0, omega1, omega2, omega3, psi]
 speeds
-
-kinematical_differential_equations
-
-kane = KanesMethod(inertial_frame, coordinates, speeds, kinematical_differential_equations)
-
-loads = [lower_leg_left_grav_force,
-         upper_leg_left_grav_force,
-         hip_grav_force, 
-         upper_leg_right_grav_force,
-         lower_leg_right_grav_force,
-         lower_leg_left_torque,
-         upper_leg_left_torque,
-         hip_left_torque,
-         hip_right_torque,
-         upper_leg_right_torque,
-         lower_leg_right_torque]
-loads
-
-bodies = [lower_leg_left, upper_leg_left, hip, upper_leg_right, lower_leg_right]
-bodies
-
-fr, frstar = kane.kanes_equations(loads, bodies)
-
-trigsimp(fr + frstar)
-
-mass_matrix = trigsimp(kane.mass_matrix_full)
-#print(mass_matrix)
-
-forcing_vector = trigsimp(kane.forcing_full)
-#print(forcing_vector)
+#
+#kinematical_differential_equations
+#
+#kane = KanesMethod(inertial_frame, coordinates, speeds, kinematical_differential_equations)
+#
+#loads = [lower_leg_left_grav_force,
+#         upper_leg_left_grav_force,
+#         hip_grav_force, 
+#         upper_leg_right_grav_force,
+#         lower_leg_right_grav_force,
+#         lower_leg_left_torque,
+#         upper_leg_left_torque,
+#         hip_left_torque,
+#         hip_right_torque,
+#         upper_leg_right_torque,
+#         lower_leg_right_torque]
+#loads
+#
+#bodies = [lower_leg_left, upper_leg_left, hip, upper_leg_right, lower_leg_right]
+#bodies
+#
+#print("evaluating kanes equation")
+#fr, frstar = kane.kanes_equations(loads, bodies)
+#
+##print("simplifying kanes equation")
+##trigsimp(fr + frstar)
+#
+#print("simplifying mass_matrix")
+#mass_matrix = trigsimp(kane.mass_matrix_full)
+##print(mass_matrix)
+#
+#print("simplifying forcing_vector")
+#forcing_vector = trigsimp(kane.forcing_full)
+##print(forcing_vector)
--- python/PaBiRoboy_dynamics_simulation.py
+++ python/PaBiRoboy_dynamics_simulation.py
@@ -2,6 +2,7 @@
 from PaBiRoboy_dynamics_derivation import *
 #%%
 from scipy.integrate import odeint
+from sympy.utilities.codegen import codegen
 from pydy.codegen.ode_function_generators import generate_ode_function
 from matplotlib.pyplot import plot, legend, xlabel, ylabel, rcParams
 rcParams['figure.figsize'] = (14.0, 6.0)
@@ -42,47 +43,51 @@ numerical_constants
 
 specified = [ankle_torque0, knee_torque0, hip_torque0, hip_torque1, knee_torque1, ankle_torque1]
 #%%
-x0 = zeros(8)
+x0 = zeros(10)
 x0
 
-x0[0] = deg2rad(0.1)
-x0[1] = deg2rad(0.1)
-x0[2] = deg2rad(0.1)
-x0[3] = deg2rad(0.1)
+x0[0] = deg2rad(80)
+x0[1] = -deg2rad(80)
+x0[2] = -deg2rad(80)
+x0[3] = deg2rad(80)
+x0[4] = 0
 
 x0
 
 numerical_specified = zeros(6)
+#%%
+rad2deg(-3.14/2.0)
 #%% Jacobian
 print ('calculating jacobian')
-F0 = ankle_left.pos_from(origin).express(inertial_frame).simplify().to_matrix(inertial_frame)
+F0 = ankle_right.pos_from(origin).express(inertial_frame).simplify().to_matrix(inertial_frame)
 F0 = Matrix([F0[0], F0[1]])
 F0
-##%%
-#F00 = ankle_left.vel(inertial_frame).simplify().to_matrix(inertial_frame)
-#F00 = Matrix([F00[0], F00[1]])
-#F00
 #%%
-F1 = ankle_right.pos_from(origin).express(inertial_frame).simplify().to_matrix(inertial_frame)
+L = 1
+#constraint = F0 - Matrix([L,0])
+J_ankleRight = F0.jacobian([theta0, theta1, theta2, theta3, phi])
+J_ankleRight.shape
+#%%
+F1 = hip_center.pos_from(origin).express(inertial_frame).simplify().to_matrix(inertial_frame)
 F1 = Matrix([F1[0], F1[1]])
 F1
 #%%
-J_ankleLeft = F0.jacobian([theta0, theta1, theta2, theta3])
-J_ankleLeft
+J_hipCenter = F1.jacobian([theta0, theta1, theta2, theta3, phi])
+J_hipCenter
 #%%
-J_ankleRight = F1.jacobian([theta0, theta1, theta2, theta3])
-J_ankleRight
-#%%
-J = J_ankleLeft.col_join(J_ankleRight)
+J = J_ankleRight.col_join(J_hipCenter)
 J
-##
-values = {lower_leg_length: 0.4, upper_leg_length: 0.54, hip_length: 0.2, theta0: x0[0], theta1: x0[1], theta2: x0[2], theta3: x0[3]}
+#%%
+values = {lower_leg_length: 0.4, upper_leg_length: 0.54, hip_length: 0.2, theta0: x0[0], theta1: x0[1], theta2: x0[2], theta3: x0[3], phi: x0[4]}
 Jpinv = J.subs(values).evalf().pinv()
 Jpinv
+#%%
+F2 = F0.col_join(F1)
+F2
 #np.linalg.pinv(np.array(J.subs(values).evalf()).astype(float))
 #%% 
-frames_per_sec = 60
-final_time = 5
+frames_per_sec = 30
+final_time = 1
 
 t = linspace(0.0, final_time, final_time * frames_per_sec)
 
@@ -100,18 +105,24 @@ t = linspace(0.0, final_time, final_time * frames_per_sec)
 #y
 
 #%%
-from sympy import lambdify, solve
-M_func = lambdify(coordinates + speeds + constants + specified, mass_matrix) # Create a callable function to evaluate the mass matrix 
-f_func = lambdify(coordinates + speeds + constants + specified, forcing_vector)     # Create a callable function to evaluate the forcing vector       
+#from sympy import lambdify, solve
+#M_func = lambdify(coordinates + speeds + constants + specified, mass_matrix) # Create a callable function to evaluate the mass matrix 
+#f_func = lambdify(coordinates + speeds + constants + specified, forcing_vector)     # Create a callable function to evaluate the forcing vector       
 #%%
-kp = 10
+kp = np.eye(4)
+kp[0,0] = 10
+kp[1,1] = 10
+kp[2,2] = 10
+kp[3,3] = 10
+kp
+#%%
 kpN = np.zeros((4,4),dtype=float)
-kpN[0,0] = 1
+kpN[1,1] = 10
 kpN
-x_des = Matrix([0.4,-0.3,-0.2,-0.5])
+x_des = Matrix([L,0,0.7,0.1])
 x_des.shape
 #%%
-
+i = 0
 def right_hand_side(x, t, args):
     """Returns the derivatives of the states.
 
@@ -130,33 +141,31 @@ def right_hand_side(x, t, args):
         The derivative of the state.
     
     """
+    global i
     r = 0.0                                  # The input force is always zero     
     arguments = np.hstack((x, args))      # States, input, and parameters
     values = {lower_leg_length: 0.4, upper_leg_length: 0.54, hip_length: 0.2, 
-              theta0: x[0], theta1: x[1], theta2: x[2], theta3: x[3],
-                omega0: 0, omega1: 0, omega2: 0, omega3: 0}
-#    Jpinv = np.linalg.pinv(np.array(J.subs(values).evalf()).astype(float))
+              theta0: x[0], theta1: x[1], theta2: x[2], theta3: x[3], phi: x[4],
+                omega0: 0, omega1: 0, omega2: 0, omega3: 0, psi: 0}
     Jpinv = J.subs(values).evalf().pinv()
 #    N=np.eye(4)-Jpinv*J.subs(values).evalf()
     x_current = F0.subs(values).evalf().col_join(F1.subs(values).evalf())
-    dq = np.array(Jpinv*(kp*( x_des - x_current ))).astype(float).T[0]# + kp*( x_des - x_current ))+ N*(kpN*(Matrix([0,0,0,0,0])-Matrix([x[0],x[1],x[2],x[3],x[4]])))).astype(float).T[0]
-    print(x_current)
-    dq = np.hstack((dq,np.zeros(4,dtype=float)))
+#    x_des[0] = x_current[0]
+#    x_des[2] = x_current[2]
+    dq = np.array(Jpinv*(kp*( x_des - x_current ))).astype(float).T[0]# + N*(kpN*(Matrix([0,-deg2rad(80),0,0,0])-Matrix([x[0],x[1],x[2],x[3])))).astype(float).T[0]
+    dq = np.hstack((dq,np.zeros(5,dtype=float)))
+    if i%10==0:
+        print(x_current)
+    i = i+1
     return dq
-##    print(x)
-
 #    dq = np.array(np.linalg.solve(M_func(*arguments),  # Solving for the derivatives
 #                  f_func(*arguments))).T[0]
 #    return dq
     
     
-
-#%%
-args = (np.hstack((numerical_constants,numerical_specified,)),)
-args[0].shape
-
 #%%
 print('integrating')
+args = (np.hstack((numerical_constants,numerical_specified,)),)
 y = odeint(right_hand_side, x0, t, args)#,full_output = 1,mxstep=1000000
 #y = np.hstack((y,np.zeros(y.shape)))
 print(y)
@@ -165,20 +174,104 @@ print('done integrating')
 values = {lower_leg_length: 0.4, upper_leg_length: 0.54, hip_length: 0.2, 
               theta0: deg2rad(90), theta1: deg2rad(0), theta2: deg2rad(160), theta3: deg2rad(0)}
 F0.subs(values).evalf()        
-##%%
-#
-#Jpinv = J.subs(values).pinv()
-#Jpinv
-###    N=np.eye(4)-Jpinv*J;
-#x_current = F0.subs(values).evalf()
-#x_current
-#dq = np.array(Jpinv*(x_current + kp*(Matrix([-0.0632292870069145, -0.925719287831476]) - x_current ))).astype(float)
-#dq
 #%%
-#Jpinv = np.linalg.pinv(np.array(J.subs(values).evalf()).astype(float))
-#N=np.eye(5)-Jpinv*J
-#N.shape
-#x_current = F0.subs(values).evalf()
-#values = {hip_offset_x: 0, hip_offset_y: 1.0, lower_leg_length: 0.4, upper_leg_length: 0.54, hip_length: 0.2, theta0: 0.0, theta1: 0.1, theta2: 0.1, theta3: 0.1, theta4: 0.1}
-#Jpinv = np.linalg.pinv(np.array(J.subs(values).evalf()).astype(float))
-#np.array(Jpinv*(x_current + kp*(Matrix([-0.0632292870069145, -0.93800166611121]) - x_current )) + N*(kpN*(Matrix([0,0,0,0,0])-Matrix([0,0,0,0,0])))).astype(float).T[0]
+#%% Plot
+print("Plotting")
+plot(t, rad2deg(y[:, :5]))
+xlabel('Time [s]')
+ylabel('Angle [deg]')
+legend(["${}$".format(vlatex(c)) for c in coordinates])
+                     
+#plot(t, rad2deg(y[:, 5:]))
+#xlabel('Time [s]')
+#ylabel('Angular Rate [deg/s]')
+#legend(["${}$".format(vlatex(s)) for s in speeds])
+
+#%% Visualization
+print("Visualize")
+# external
+from pydy.viz.shapes import Cylinder, Sphere
+from pydy.viz.scene import Scene
+from pydy.viz.visualization_frame import VisualizationFrame
+
+# A cylinder will be attached to each link and a sphere to each bob for the
+# visualization.
+lengths = [lower_leg_length, upper_leg_length, hip_length, hip_length, hip_length, upper_leg_length, lower_leg_length]
+bodies = [lower_leg_left, upper_leg_left, hip, hip, hip, upper_leg_right, lower_leg_right]
+
+viz_frames = []
+colors = ['yellow','green','red','red','red','green','blue']
+
+for i, (body, particle, mass_center) in enumerate(zip(bodies, particles, mass_centers)):
+#        body_shape = Cylinder(name='cylinder{}'.format(i),
+#                              radius=0.05,
+#                              length=lengths[i],
+#                              color='red')
+#    
+#        viz_frames.append(VisualizationFrame('link_frame{}'.format(i), body,
+#                                             body_shape))
+            
+    particle_shape = Sphere(name='sphere{}'.format(i),
+                            radius=0.06,
+                            color=colors[i])
+                            
+    
+    viz_frames.append(VisualizationFrame('particle_frame{}'.format(i),
+                                         body.frame,
+                                         particle,
+                                         particle_shape))
+                                         
+    mass_center_shape = Sphere(name='sphere{}'.format(i),
+                            radius=0.02,
+                            color='black')
+                                         
+    viz_frames.append(VisualizationFrame('mass_center_frame{}'.format(i),
+                                         body.frame,
+                                         mass_center,
+                                         mass_center_shape))
+                                             
+target_shape = Sphere(name='sphere{}'.format(i),
+                            radius=0.02,
+                            color='green')    
+
+target_right_leg = Point('target_right_leg')
+target_right_leg.set_pos(origin, (x_des[2] * inertial_frame.x)+(x_des[3] * inertial_frame.y))                              
+
+                                     
+viz_frames.append(VisualizationFrame('target_frame_right',
+                                     inertial_frame,
+                                     target_right_leg,
+                                     target_shape))
+
+## Now the visualization frames can be passed in to create a scene.
+scene = Scene(inertial_frame, origin, *viz_frames)
+
+# Provide the data to compute the trajectories of the visualization frames.
+scene.constants = dict(zip(constants, numerical_constants))
+scene.states_symbols = coordinates+speeds
+scene.states_trajectories = y
+
+scene.display()
+#%% export to c header
+t = symbols('t')
+a0 = ankle_left.pos_from(origin).express(inertial_frame).simplify().to_matrix(inertial_frame)
+k0 = knee_left.pos_from(origin).express(inertial_frame).simplify().to_matrix(inertial_frame)
+hl = hip_left.pos_from(origin).express(inertial_frame).simplify().to_matrix(inertial_frame)
+hc = hip_center.pos_from(origin).express(inertial_frame).simplify().to_matrix(inertial_frame)
+hr = hip_right.pos_from(origin).express(inertial_frame).simplify().to_matrix(inertial_frame)
+k1 = knee_right.pos_from(origin).express(inertial_frame).simplify().to_matrix(inertial_frame)
+a1 = ankle_right.pos_from(origin).express(inertial_frame).simplify().to_matrix(inertial_frame)
+[(c_name, c_code), (h_name, c_header)] = codegen( 
+    [("Jacobian",J), 
+     ("ankle_right_hip_center",F2), 
+     ("ankle_left", a0),
+     ("knee_left", k0),
+     ("hip_left", hl),
+     ("hip_center", hc),
+     ("hip_right", hr),
+     ("knee_right", k1),
+     ("ankle_right",a1)
+    ] ,
+    "C", "PaBiRoboy_DanceControl", project='PaBiRoboy_DanceControl',global_vars=(t,lower_leg_length, upper_leg_length, hip_length, theta0,theta1,theta2,theta3), 
+    header=True, empty=False)
+print(c_code)
